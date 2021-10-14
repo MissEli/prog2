b2644aca2797dc6bb7181661c07ecddb8000f793
Elizaveta Yakovleva <elya8222@atterbom.it.uu.se>
2021-10-14 09:04:43 +0200

    sista fix

--- MA4_files/MA4_2.py
+++ MA4_files/MA4_2.py
@@ -4,12 +4,13 @@ from integer import Integer
 from time import perf_counter as pc
 
 def main():
-	start = pc()
-	f = Integer(30)
-	f.fib()
-	print(f.get())
-	end = pc()
-	print(f"Process took {round(end-start,2)} seconds")
+	for n in [30,32,35,37,40,43,45]:
+		start = pc()
+		f = Integer(n)
+		print(f.fib())
+		print(f.get())
+		end = pc()
+		print(f"Process took {round(end-start,2)} seconds")
 
 if __name__ == '__main__':
 	main()
